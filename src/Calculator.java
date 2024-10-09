d11fe2ac3975f8148be1fc1228a6b22d6d359563
bemeritus <bemerituss@gmail.com>
2024-10-09 16:33:42 +0500

    add: frame, button and panel

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,5 +1,28 @@
-public class Calculator{
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+
+public class Calculator implements ActionListener {
+
+    JFrame frame;
+    JTextField textField;
+    JButton[] numberButtons = new JButton[10];
+    JButton[] functionButtons = new JButton[8];
+    JButton addButton, subButton, mulButton, divButton;
+    JButton decButton, equButton, delButton, clrButton;
+    JPanel panel;
+
+    Calculator(){
+
+    }
+
     public static void main(String[] args){
-        System.out.println("Calculator");
+        Calculator calc = new Calculator();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
     }
 }
