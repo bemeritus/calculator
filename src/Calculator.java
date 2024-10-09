963e8c243defd5b64228026675bb4951ac0131d0
bemeritus <bemerituss@gmail.com>
2024-10-09 16:49:31 +0500

    add: textField

--- src/Calculator.java
+++ src/Calculator.java
@@ -13,8 +13,24 @@ public class Calculator implements ActionListener {
     JButton decButton, equButton, delButton, clrButton;
     JPanel panel;
 
-    Calculator(){
+    Font myFont = new Font("Ink Free", Font.BOLD, 30);
 
+    double num1=0, num2=0, num3=0;
+    char operator;
+
+    Calculator(){
+        frame = new JFrame("Calculator");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(420, 550);
+        frame.setLayout(null);
+
+        textField = new JTextField();
+        textField.setBounds(50, 25, 300, 50);
+        textField.setFont(myFont);
+        textField.setEditable(false);
+
+        frame.add(textField);
+        frame.setVisible(true);
     }
 
     public static void main(String[] args){
